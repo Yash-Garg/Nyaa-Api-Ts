9fd4d69317939b609f7ff08f0930a1b04c2ca4cb
Yash-Garg <yash.garg@outlook.in>
2022-09-11 23:09:44 +0530

    feat: add empty methods to API

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import { listen, Router } from "worktop";
 import * as CORS from "worktop/cors";
-import { aliveHandler } from "./routes";
+import * as Routes from "./routes";
 
 const API = new Router();
 
 API.prepare = CORS.preflight({
-  origin: "*", // allow any `Origin` to connect
-  headers: ["Cache-Control", "Content-Type", "X-Count"],
-  methods: ["GET", "HEAD", "PUT", "POST", "DELETE"],
+  origin: "*",
+  headers: ["Cache-Control", "Content-Type"],
+  methods: ["GET"],
 });
 
-API.add("GET", "/", aliveHandler);
+API.add("GET", "/", Routes.PingApi);
+API.add("GET", "/id/:id", Routes.GetInfoFromID);
+API.add("GET", "/user/:username", Routes.GetUserUploads);
+API.add("GET", "/:category/:sub_category?", Routes.GetCategoryTorrents);
 
 listen(API.run);
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Handler } from "worktop";
 
-export const aliveHandler: Handler = function (_, res) {
+export const PingApi: Handler = function (_, res) {
   res.send(200, "Nyaa API v2 // Alive");
 };
+
+export const GetInfoFromID: Handler = function (req, res) {};
+
+export const GetUserUploads: Handler = function (req, res) {};
+
+export const GetCategoryTorrents: Handler = function (req, res) {};
