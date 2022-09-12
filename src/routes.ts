3b7d31be2de78df95df8199c279747f389e40125
Yash-Garg <yash.garg@outlook.in>
2022-09-12 20:42:57 +0530

    chore: use try-catch and return 404 on error

--- src/routes.ts
+++ src/routes.ts
@@ -3,45 +3,54 @@ import * as Scrapers from "./scrapers";
 import { Constants } from "./constants";
 import * as Utils from "./utils";
 
+const baseUrl = Constants.NyaaAltUrl;
+
 export class Handlers {
   static Ping: Handler = function (_, res) {
     res.send(200, "Nyaa API v2 // Alive");
   };
 
   static GetInfoFromID: Handler = async function (req, res) {
-    const { id } = req.params;
-    // const baseUrl = await checkNyaaUrl();
-    const searchUrl = Constants.NyaaAltUrl + "/view/" + id;
+    try {
+      const { id } = req.params;
+      const searchUrl = baseUrl + "/view/" + id;
 
-    await Scrapers.fileInfoScraper(res, searchUrl);
+      await Scrapers.fileInfoScraper(res, searchUrl);
+    } catch (error) {
+      res.send(404, "Not Found");
+    }
   };
 
   static GetUserUploads: Handler = async function (req, res) {
-    const username = req.params.username;
-    const queryParams = Utils.getSearchParameters(req);
+    try {
+      const username = req.params.username;
+      const queryParams = Utils.getSearchParameters(req);
 
-    const searchUrl = `${
-      Constants.NyaaAltUrl
-    }/user/${username}?q=${queryParams.query.trim()}&p=${queryParams.page}&s=${
-      queryParams.sort
-    }&o=${queryParams.order}&f=${queryParams.filter}`;
+      const searchUrl = `${baseUrl}/user/${username}?q=${queryParams.query.trim()}&p=${
+        queryParams.page
+      }&s=${queryParams.sort}&o=${queryParams.order}&f=${queryParams.filter}`;
 
-    await Scrapers.scrapeNyaa(res, searchUrl);
+      await Scrapers.scrapeNyaa(res, searchUrl);
+    } catch (error) {
+      res.send(404, "Not Found");
+    }
   };
 
   static GetCategoryTorrents: Handler = async function (req, res) {
-    const cat = req.params.category;
-    const subCat = req.params.subcategory;
+    try {
+      const cat = req.params.category;
+      const subCat = req.params.subcategory;
 
-    const category = Utils.getCategoryID(cat, subCat);
-    const queryParams = Utils.getSearchParameters(req);
+      const category = Utils.getCategoryID(cat, subCat);
+      const queryParams = Utils.getSearchParameters(req);
 
-    const searchUrl = `${
-      Constants.NyaaAltUrl
-    }?q=${queryParams.query.trim()}&c=${category}&p=${queryParams.page}&s=${
-      queryParams.sort
-    }&o=${queryParams.order}&f=${queryParams.filter}`;
+      const searchUrl = `${baseUrl}?q=${queryParams.query.trim()}&c=${category}&p=${
+        queryParams.page
+      }&s=${queryParams.sort}&o=${queryParams.order}&f=${queryParams.filter}`;
 
-    await Scrapers.scrapeNyaa(res, searchUrl);
+      await Scrapers.scrapeNyaa(res, searchUrl);
+    } catch (error) {
+      res.send(404, "Not Found");
+    }
   };
 }
